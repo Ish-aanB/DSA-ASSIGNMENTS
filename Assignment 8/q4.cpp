bafdf2b5b6dbe14d4fbf2b6c769538fd59faefbe
Ish-aanB <ibhalla_be24@thapar.edu>
2025-12-05 03:15:05 +0530

    q4.cpp

--- Assignment 8/q4.cpp
+++ Assignment 8/q4.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct TreeNode {
-    TreeNode* left;
-    TreeNode* right;
+struct Node {
+    Node* left;
+    Node* right;
     int val;
-    TreeNode(int value=0) {
+    Node(int value=0) {
         val = value;
         left = nullptr;
         right = nullptr;
     }
 };
 
-bool isBstHelper(TreeNode* root, int min, int max) {
+bool isBstHelper(Node* root, int min, int max) {
     if (!root) return true;
 
     if (root->val < min || root->val > max) return false;
@@ -20,20 +20,20 @@ bool isBstHelper(TreeNode* root, int min, int max) {
     
 }
 
-bool isBST(TreeNode* root) {
+bool isBST(Node* root) {
     return isBstHelper(root, INT_MIN, INT_MAX);
 }
 int main() {
-    TreeNode* root = new TreeNode(10);
+    Node* root = new Node(10);
 
-    root->left = new TreeNode(11);
-    root->right = new TreeNode(20);
+    root->left = new Node(11);
+    root->right = new Node(20);
 
-    root->left->left = new TreeNode(3);
-    root->left->right = new TreeNode(7);
+    root->left->left = new Node(3);
+    root->left->right = new Node(7);
 
-    root->right->left = new TreeNode(15);
-    root->right->right = new TreeNode(30);
+    root->right->left = new Node(15);
+    root->right->right = new Node(30);
 
     cout << isBST(root);
 
